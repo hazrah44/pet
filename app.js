ab19a5311af4a34cb556864b9641f9cb5ec44ded
hazrahothman <77723723+hazrahothman@users.noreply.github.com>
2021-12-14 19:08:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-
 const app = Vue.createApp({
-
-data () {
+  data() {
     return {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'John@gmail.com',
-        gender: 'male',
-        picture: 'https://randomuser.me/api/portraits/men/10.jpg',
-    }
-},
-methods: {
-    getUser() {
-        this.firstName = 'Sam'
-        this.lastName = 'Smith'
-        this.email = 'sam@gmail.com'
-        this.gender = 'female'
-        this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
-     },
-},
-})
+      firstName: "John",
+      lastName: "Doe",
+      email: "John@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
 
+      console.log(results);
 
-app.mount('#app')
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.age = results[0].age;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+
+app.mount("#app");
